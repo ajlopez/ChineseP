97cb0d10bbd7606131554d2550d00287d148d781
ajlopez <webmaster@ajlopez.com>
2016-07-19 12:47:19 -0300

    add element

--- test/elements.js
+++ test/elements.js
@@ -9,3 +9,18 @@ exports['get empty list'] = function(test) {
     test.equal(list.length, 0);
 }
 
+exports['add element'] = function(test) {
+    var element = {
+        translation: "I'm fine"
+    };
+    
+    elements.add(element);
+    
+    var list = elements.list();
+    
+    test.ok(list);
+    test.ok(Array.isArray(list));
+    test.equal(list.length, 1);
+    test.equal(list[0].translation, "I'm fine");
+}
+
