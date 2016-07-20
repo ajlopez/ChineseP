32c6823b0aab702b620d54a1a84340dd530205c2
ajlopez <webmaster@ajlopez.com>
2016-07-20 16:04:27 -0300

    get and retrieve element

--- test/elements.js
+++ test/elements.js
@@ -24,3 +24,18 @@ exports['add element'] = function(test) {
     test.equal(list[0].translation, "I'm fine");
 }
 
+exports['add and retrieve element'] = function(test) {
+    var element = {
+        translation: "wo",
+        type: "pronoun"
+    };
+    
+    elements.add(element);
+    
+    var list = elements.list({ type: "pronoun" });
+    
+    test.ok(list);
+    test.ok(Array.isArray(list));
+    test.equal(list.length, 1);
+    test.equal(list[0].translation, "wo");
+}
