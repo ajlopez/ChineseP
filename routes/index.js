cb2be9f2845aa87b8d47d5c1847264890d3c01d6
ajlopez <webmaster@ajlopez.com>
2017-06-25 19:36:47 -0300

    First page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var chinese = require('../lib/chinese');
+
+chinese.load('../data/elements.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	var items = chinese.select({ character: true }, 5);
+	var selected = Math.floor(Math.random() * items.length);
+	res.render('index', { title: 'Chinese Practice', items: items, selected: selected });
 });
 
 module.exports = router;
